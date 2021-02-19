dbd5372f9ff05a6ac17e5e66e561390a34324de0
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2021-02-19 17:36:55 -0300

    Lava
    Alteração no nome das variáveis.

--- lava.py
+++ lava.py
@@ -2,14 +2,14 @@ import pygame
 pygame.init()
 
 # Classe da lava, obstáculo do jogo
-tile_size = 50
+tileSize = 50
 
 
 class Lava(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, daltonism):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('img/lava.png')
-        self.image = pygame.transform.scale(img, (tile_size, tile_size // 2))
+        img = pygame.image.load(f'img/{daltonism}/lava.png')
+        self.image = pygame.transform.scale(img, (tileSize, tileSize // 2))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
