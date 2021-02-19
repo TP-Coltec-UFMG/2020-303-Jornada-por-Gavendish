21ff5a53c19c0be0f626330c2c35401ba4a8d7cb
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2021-02-19 17:37:19 -0300

    Mundo
    Alteração no nome das variáveis.

--- mundo.py
+++ mundo.py
@@ -10,77 +10,77 @@ screen = pygame.display.set_mode((900, 600))
 
 
 # Classe do mundo
-tile_size = 50
+tileSize = 50
 
 
 class World:
-    def __init__(self, data, enemy_group, lava_group, coin_group, exit_group, key_group):
+    def __init__(self, data, enemy_group, lava_group, coin_group, exit_group, key_group, daltonism):
         self.tile_list = []
         # Carregando as imagens de blocos
-        dirt_img = pygame.image.load('img/dirt.jpg')
-        grass_img = pygame.image.load('img/grass.jpg')
-        platform_img = pygame.image.load('img/platform.jpg')
-        platformwograss_img = pygame.image.load('img/platform2.jpg')
-        row_count = 0
+        dirtImg = pygame.image.load(f'img/{daltonism}/dirt.jpg')
+        grassImg = pygame.image.load(f'img/{daltonism}/grass.jpg')
+        platformImg = pygame.image.load(f'img/{daltonism}/platform.jpg')
+        platformwograssImg = pygame.image.load(f'img/{daltonism}/platform2.jpg')
+        rowCount = 0
         for row in data:
-            col_count = 0
+            colCount = 0
             for tile in row:
                 if tile == '1':
-                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
+                    img = pygame.transform.scale(dirtImg, (tileSize, tileSize))
+                    imgRect = img.get_rect()
+                    imgRect.x = colCount * tileSize
+                    imgRect.y = rowCount * tileSize
+                    tile = (img, imgRect)
                     self.tile_list.append(tile)
                 if tile == '2':
-                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
+                    img = pygame.transform.scale(grassImg, (tileSize, tileSize))
+                    imgRect = img.get_rect()
+                    imgRect.x = colCount * tileSize
+                    imgRect.y = rowCount * tileSize
+                    tile = (img, imgRect)
                     self.tile_list.append(tile)
                 if tile == '3':
-                    img = pygame.transform.scale(platform_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
+                    img = pygame.transform.scale(platformImg, (tileSize, tileSize))
+                    imgRect = img.get_rect()
+                    imgRect.x = colCount * tileSize
+                    imgRect.y = rowCount * tileSize
+                    tile = (img, imgRect)
                     self.tile_list.append(tile)
                 if tile == '4':
-                    img = pygame.transform.scale(platformwograss_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
+                    img = pygame.transform.scale(platformwograssImg, (tileSize, tileSize))
+                    imgRect = img.get_rect()
+                    imgRect.x = colCount * tileSize
+                    imgRect.y = rowCount * tileSize
+                    tile = (img, imgRect)
                     self.tile_list.append(tile)
                 if tile == '5':
-                    enemy = Enemy(col_count * tile_size, row_count * tile_size + 12)
+                    enemy = Enemy(colCount * tileSize, rowCount * tileSize + 12, daltonism)
                     enemy_group.add(enemy)
                 if tile == '6':
-                    lava = Lava(col_count * tile_size, row_count * tile_size + (tile_size // 2))
+                    lava = Lava(colCount * tileSize, rowCount * tileSize + (tileSize // 2), daltonism)
                     lava_group.add(lava)
                 if tile == '7':
-                    coin = Coin(col_count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size // 2))
+                    coin = Coin(colCount * tileSize + (tileSize // 2), rowCount * tileSize + (tileSize // 2), daltonism)
                     coin_group.add(coin)
                 if tile == '8':
-                    exitdoor = Exit(col_count * tile_size, row_count * tile_size - (tile_size // 2))
+                    exitdoor = Exit(colCount * tileSize, rowCount * tileSize - (tileSize // 2), daltonism)
                     exit_group.add(exitdoor)
                 if tile == '9':
-                    key = Key(col_count * tile_size, row_count * tile_size - (tile_size // 2))
+                    key = Key(colCount * tileSize, rowCount * tileSize - (tileSize // 2), daltonism)
                     key_group.add(key)
-                col_count += 1
-            row_count += 1
+                colCount += 1
+            rowCount += 1
 
     def draw(self):
         for tile in self.tile_list:
             screen.blit(tile[0], tile[1])
 
     @staticmethod
-    def reset_level(level, player, enemy_group, lava_group, exit_group, coin_group, key_group):
-        player.__init__(100, 600 - 130)
-        enemy_group.empty()
-        lava_group.empty()
-        exit_group.empty()
+    def reset_level(level, player, enemyGroup, lavaGroup, exitGroup, coinGroup, keyGroup, daltonism):
+        player.__init__(100, 600 - 130, daltonism)
+        enemyGroup.empty()
+        lavaGroup.empty()
+        exitGroup.empty()
 
         # load in level data and create world
         archive = open(f'levels/level{level}.txt', 'r')
@@ -91,5 +91,5 @@ class World:
         for num in range(0, 12):
             world_data.append(list(data[num]))
 
-        world = World(world_data, enemy_group, lava_group, coin_group, exit_group, key_group)
+        world = World(world_data, enemyGroup, lavaGroup, coinGroup, exitGroup, keyGroup, daltonism)
         return world
