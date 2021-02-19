1545b696d185d811b2acded46efaeecce5907657
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2021-02-19 17:38:23 -0300

    Saída
    Alteração no nome das variáveis.

--- saida.py
+++ saida.py
@@ -2,24 +2,24 @@ import pygame
 
 
 # Classe da porta de saida do nível
-tile_size = 50
+tileSize = 50
 
 
 class Exit(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, daltonism):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('img/exitlocked.png')
-        self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
+        img = pygame.image.load(f'img/{daltonism}/exitlocked.png')
+        self.image = pygame.transform.scale(img, (tileSize, int(tileSize * 1.5)))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.isLocked = True
 
-    def update(self, keyCollected):
+    def update(self, keyCollected, daltonism):
         if keyCollected == 0:
-            img = pygame.image.load('img/exitlocked.png')
-            self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
+            img = pygame.image.load(f'img/{daltonism}/exitlocked.png')
+            self.image = pygame.transform.scale(img, (tileSize, int(tileSize * 1.5)))
         if keyCollected == 1:
-            img = pygame.image.load('img/exit.png')
-            self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
+            img = pygame.image.load(f'img/{daltonism}/exit.png')
+            self.image = pygame.transform.scale(img, (tileSize, int(tileSize * 1.5)))
 
