20f95fe999ccd170a5d155f08bc1b7d5d3df89b2
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2021-02-25 15:15:42 -0300

    Chave
    Alterações nos caminhos das imagens já que elas foram transferidas para uma pasta maior (assets/).

--- chave.py
+++ chave.py
@@ -8,7 +8,7 @@ tileSize = 50
 class Key(pygame.sprite.Sprite):
     def __init__(self, x, y, daltonism):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load(f'img/{daltonism}/key.png')
+        img = pygame.image.load(f'assets/img/{daltonism}/key.png')
         self.image = pygame.transform.scale(img, (tileSize, tileSize // 2))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
