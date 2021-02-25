baf34d37502cb026cb0addc12ba6123522bcfd7d
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2021-02-25 15:17:16 -0300

    Lava
    Alterações nos caminhos das imagens já que elas foram transferidas para uma pasta maior (assets/).

--- lava.py
+++ lava.py
@@ -8,7 +8,7 @@ tileSize = 50
 class Lava(pygame.sprite.Sprite):
     def __init__(self, x, y, daltonism):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load(f'img/{daltonism}/lava.png')
+        img = pygame.image.load(f'assets/img/{daltonism}/lava.png')
         self.image = pygame.transform.scale(img, (tileSize, tileSize // 2))
         self.rect = self.image.get_rect()
         self.rect.x = x
