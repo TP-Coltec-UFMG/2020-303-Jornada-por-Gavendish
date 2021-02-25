5749feebc03f463fa8b58191952f4069373d5836
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2021-02-25 15:19:21 -0300

    Moeda
    Alterações nos caminhos das imagens já que elas foram transferidas para uma pasta maior (assets/).

--- moeda.py
+++ moeda.py
@@ -7,7 +7,7 @@ tileSize = 50
 class Coin(pygame.sprite.Sprite):
     def __init__(self, x, y, daltonism):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load(f'img/{daltonism}/coin.png')
+        img = pygame.image.load(f'assets/img/{daltonism}/coin.png')
         self.image = pygame.transform.scale(img, (tileSize // 2, tileSize // 2))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
