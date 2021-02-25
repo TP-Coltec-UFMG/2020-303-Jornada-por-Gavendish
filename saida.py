4f00c7a6732b2bbe41d08584009034334ef40e6b
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2021-02-25 15:19:42 -0300

    Saída
    Alterações nos caminhos das imagens já que elas foram transferidas para uma pasta maior (assets/).

--- saida.py
+++ saida.py
@@ -8,7 +8,7 @@ tileSize = 50
 class Exit(pygame.sprite.Sprite):
     def __init__(self, x, y, daltonism):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load(f'img/{daltonism}/exitlocked.png')
+        img = pygame.image.load(f'assets/img/{daltonism}/exitlocked.png')
         self.image = pygame.transform.scale(img, (tileSize, int(tileSize * 1.5)))
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -17,9 +17,9 @@ class Exit(pygame.sprite.Sprite):
 
     def update(self, keyCollected, daltonism):
         if keyCollected == 0:
-            img = pygame.image.load(f'img/{daltonism}/exitlocked.png')
+            img = pygame.image.load(f'assets/img/{daltonism}/exitlocked.png')
             self.image = pygame.transform.scale(img, (tileSize, int(tileSize * 1.5)))
         if keyCollected == 1:
-            img = pygame.image.load(f'img/{daltonism}/exit.png')
+            img = pygame.image.load(f'assets/img/{daltonism}/exit.png')
             self.image = pygame.transform.scale(img, (tileSize, int(tileSize * 1.5)))
 
